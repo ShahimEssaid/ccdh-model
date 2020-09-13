9096add20f65b655f5c9cb35f83c89da988e2823
Shahim Essaid <shahim@essaid.com>
2020-09-13 16:16:11 -0700

    trafix fix

--- bin/travis.sh
+++ bin/travis.sh
@@ -4,3 +4,18 @@ set -e
 set -u
 set -o pipefail
 set -o noclobber
+
+# See http://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+GIT_ROOT="$(dirname "$DIR")"
+
+cd $GIT_ROOT
+
+bin/install_dependencies.sh
